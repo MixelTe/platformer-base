72cc1fb634e6f2fe20f2560459872e7398a159a9
Mike <63655075+MixelTe@users.noreply.github.com>
2020-09-08 12:49:58 +0300

    change level01

--- ts/levels/level01.ts
+++ ts/levels/level01.ts
@@ -5,15 +5,19 @@ import { Platform_Movable } from "../platforms/movable.js";
 export class Level1 extends Level
 {
 	public platforms = [
-		new Platform(100, 0, 500, 10),
-		new Platform(350, 10, 50, 30),
-		new Platform(200, 50, 200, 40),
-		new Platform_Movable(300, 90, 200, 40, 5, 0, 400, "y"),
-	];
+		new Platform(100, 90, 100, 10),
+		new Platform(400, 180, 200, 10),
+
+
+		new Platform(0, 0, 800, 10),
+		new Platform(0, 0, 10, 600),
+		new Platform(790, 0, 10, 600),
+		new Platform(0, 590, 800, 10),
+		];
 
 	constructor()
 	{
 		super();
-		this.character.setCoords(200, 20);
+		this.character.setCoords(200, 120);
 	}
 }
